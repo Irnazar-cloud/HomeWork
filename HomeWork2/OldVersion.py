5bb03cb1704b009807daee3756a1d1abd1bb4600
Irnazar-cloud <irnazar_sharipov@mail.ru>
2024-12-30 19:52:28 +1100

    Add files via upload

---
+++ HomeWork2/OldVersion.py
@@ -0,0 +1,8 @@
+# 1st program
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(99.9 > 99.8 and 1000 != 1000.1)
+#"3rd program"
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+#"4rd program"
+print(13.42 * 100 % 100 == int(42.13) or 13.42 * 100 // 100 == 42.13 * 100 % 100)
